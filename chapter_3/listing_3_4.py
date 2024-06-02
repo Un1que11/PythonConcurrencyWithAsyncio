c9db727f31ae750a9eda31c2fdf77f765ef3987c
Mikhail Orlov <crazyminer398@gmail.com>
2024-06-02 20:44:13 +0200

    added chapter 3 listings

---
+++ chapter_3/listing_3_4.py
@@ -0,0 +1,7 @@
+import socket
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket.bind(('localhost', 8000))
+server_socket.listen()
+server_socket.setblocking(False)
