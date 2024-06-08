8aae2f4e5df9415ae39737728c89be61a00febcc
Mikhail Orlov <crazyminer398@gmail.com>
2024-06-08 15:07:05 +0200

    added chapter 6 listing

---
+++ chapter_6/listing_6_12.py
@@ -0,0 +1,20 @@
+from multiprocessing import Process, Value, Array
+
+
+def increment_value(shared_int: Value):
+    shared_int.get_lock().acquire()
+    shared_int.value = shared_int.value + 1
+    shared_int.get_lock().release()
+
+
+if __name__ == '__main__':
+    for _ in range(100):
+        integer = Value('i', 0)
+        procs = [Process(target=increment_value, args=(integer,)),
+                 Process(target=increment_value, args=(integer,))]
+
+        [proc.start() for proc in procs]
+        [proc.join() for proc in procs]
+
+        print(integer.value)
+        assert integer.value == 2
